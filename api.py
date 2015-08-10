9f20c0958cfa22ba87a6743a46c381c5678d4e58
Ryan K <Fondor1@users.noreply.github.com>
2015-08-09 22:06:46 -0600

    Updated api.py to include an Item class to store retrieved item details. Also added api calls to pull down all bank materials and items.

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 __author__ = 'rakai'
 
 from guildwars2api.v2.client import GuildWars2API
+# import sqlite3
 
 
 class Item:
@@ -15,12 +16,14 @@ if __name__ == '__main__':
     # rayal's key: 81668F5F-5866-8F41-81B8-6DF706417B5993297C30-5BAE-44E3-8715-5F94D84AFD8A
     apiv2 = GuildWars2API(api_key='81668F5F-5866-8F41-81B8-6DF706417B5993297C30-5BAE-44E3-8715-5F94D84AFD8A')
 
-    # Test item:
-    # i = {'icon': 'https://render.guildwars2.com/file/65A0C7367206E6CE4EC7C8CBE07EABAE0191BFBA/561548.png', 'details': {'infusion_slots': [], 'weight_class': 'Medium', 'type': 'Helm', 'defense': 97, 'suffix_item_id': 24696, 'infix_upgrade': {'attributes': [{'attribute': 'Healing', 'modifier': 60}, {'attribute': 'Power', 'modifier': 43}, {'attribute': 'Toughness', 'modifier': 43}]}, 'secondary_suffix_item_id': ''}, 'id': 123, 'restrictions': [], 'flags': ['Activity', 'Wvw', 'Dungeon', 'Pve', 'SoulBindOnUse'], 'vendor_value': 330, 'description': '', 'game_types': ['Activity', 'Wvw', 'Dungeon', 'Pve'], 'rarity': 'Exotic', 'name': "Zho's Mask", 'type': 'Armor', 'level': 80, 'default_skin': 95}
-    # item = Item(i)
+    # List if ids
+    bank_materials = [item['id'] for item in apiv2.bank_materials.get_all()]
+    bank_items = [item['id'] for item in apiv2.bank.get_all() if item is not None]
+    bank_all = bank_materials + bank_items
 
-    bank_materials = list(apiv2.bank_materials.get_all())
+    # Convert those ids to Items
+    # This should work but fails because it tries to look up a non-existing item id
+    items = [Item(item) for item in apiv2.items.get(ids=bank_materials)]
+    print(list(item.name for item in items))
 
-    for material in bank_materials:
-        # Very slow
-        print('item:', apiv2.items.get(id=material['id'])['name'], 'quantity:', material['count'])
+    # all_recipes = list(apiv2.recipes_search(input=bank_items+bank_materials))
